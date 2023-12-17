4e7054b55358d82360aba97819196963f66b6453
Vishesh Vijayvargiya <vijay.vishu03@gmail.com>
2023-12-17 17:15:13 +0530

    project complete

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import Hero from "./Hero";
+
+export {
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+};
